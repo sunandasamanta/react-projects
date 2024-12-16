369b6698884e56b7dbbe38a32f00ecccbf096b05
sunandasamanta <samantasunanda724@gmail.com>
2024-12-16 23:11:44 +0530

    update background color to black and add new color buttons

--- bg-changer/src/App.jsx
+++ bg-changer/src/App.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react'
 
 function App() {
-  const [backgroundColor, setBackgroundColor] = useState('white')
+  const [backgroundColor, setBackgroundColor] = useState('black')
   return (
     <main className={`h-screen flex justify-center items-center text-white bg-${backgroundColor}`}>
-      <div className="bg-white flex text-white font-bold px-4 py-2 gap-4 max-w-[800px] rounded-lg">
+      <div className="bg-gray-300 shadow-md flex text-white font-bold px-4 py-3 gap-4 max-w-[800px] rounded-lg absolute bottom-0 mb-12">
         <button
           className="bg-red-800 hover:bg-red-700 py-2 px-4 rounded"
           onClick={() => setBackgroundColor('red-800')}
@@ -28,6 +28,18 @@ function App() {
           onClick={() => setBackgroundColor('yellow-500')}
         >
           Yellow
+        </button>
+        <button
+          className="bg-purple-800 hover:bg-purple-700 py-2 px-4 rounded"
+          onClick={() => setBackgroundColor('purple-800')}
+        >
+          Purple
+        </button>
+        <button
+          className="bg-indigo-800 hover:bg-indigo-700 py-2 px-4 rounded"
+          onClick={() => setBackgroundColor('indigo-800')}
+        >
+          Indigo
         </button>
       </div>
     </main>
