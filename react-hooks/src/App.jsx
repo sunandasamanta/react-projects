bf93f2f44c5e7681905282f2552957031da5d37f
sunandasamanta <samantasunanda724@gmail.com>
2024-12-17 23:25:25 +0530

    init

---
+++ react-hooks/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState, useEffect, useCallback } from "react";
+
+function App() {
+  
+  return (
+    <div className="min-h-screen text-gray-200">
+      <h1 className="font-bold text-3xl">
+        Hello, World! I am learning react hooks.
+      </h1>
+    </div>
+  );
+}
+
+export default App;
