b9886e3343f58806a98580b50ea39dbd6899e951
sunandasamanta <samantasunanda724@gmail.com>
2024-12-18 23:48:24 +0530

    add password generator feature with dynamic length

--- react-hooks/src/App.jsx
+++ react-hooks/src/App.jsx
@@ -1,13 +1,30 @@
 import { useState, useEffect, useCallback } from "react";
 
 function App() {
-  
+  // password generator
+  const [length, setLength] = useState(8);
+
   return (
-    <div className="min-h-screen text-gray-200">
+    <main className="min-h-screen bg-gray-950 text-gray-200">
+      <input
+        className="p-2 rounded-md"
+        type="text"
+        name=""
+        id=""
+        placeholder="Your Password!"
+      />
+      <br />
+      <input type="radio" name="" id="" />
+      <button
+        className="p-2 bg-blue-500 text-white rounded-md"
+        onClick={() => setLength(Math.floor(Math.random() * 20) + 1)}
+      >
+        Generate Password
+      </button>
       <h1 className="font-bold text-3xl">
         Hello, World! I am learning react hooks.
       </h1>
-    </div>
+    </main>
   );
 }
 
