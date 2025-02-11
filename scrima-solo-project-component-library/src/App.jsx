bb9c57608b11cf6ed97af36b100b16799172a0d4
sunandasamanta <samantasunanda724@gmail.com>
2025-02-12 00:35:52 +0530

    component library init with tailwind

---
+++ scrima-solo-project-component-library/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main>
+      <h1 className="text-3xl font-bold text-center text-gray-900">
+        Vite + React + Tailwind CSS
+      </h1>
+    </main>
+  )
+}
+
+export default App
